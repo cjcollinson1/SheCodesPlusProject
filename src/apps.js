6b2acf6ce25ee4ea1e6fc80a12ea7efc5c73c402
cjcollinson1 <105799963+cjcollinson1@users.noreply.github.com>
2022-06-30 12:10:36 +0100

    HTML Start

--- src/apps.js
+++ src/apps.js
@@ -1,3 +1,31 @@
-alert("I Work");
-alert("Github is working");
-alert("testing still woprking");
+let now = new Date();
+let selecteddate = document.getElementById("date");
+let date = now.getDate();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let months = [
+  "Jan",
+  "Feb",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "Sept",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+let year = now.getFullYear();
+
+selecteddate.innerHTML = `${day} ${month} ${date} ${year}`;
