fd691da9c05fc5b59b4f931e4317db7b99e48c17
cjcollinson1 <cjcollinson1@gmail.com>
2022-07-11 21:59:56 +0100

    Coping Code over from other VS file

--- src/apps.js
+++ src/apps.js
@@ -1,62 +1,49 @@
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerText = response.data.main.temp;
+  temperatureElement.innerText = Math.round(response.data.main.temp);
 }
 
 function displayCity(response) {
   let cityElement = document.querySelector("#city");
-  cityElement.innerText = response.data.main.name;
+  cityElement.innerText = response.data.name;
 }
-
-displayCity();
 
 function displayDescription(response) {
   let descriptionElement = document.querySelector("#description");
   descriptionElement.innerText = response.data.weather[0].description;
 }
 
-displayDescription();
-
 function displayIcon(response) {
   let iconElement = document.querySelector("#icon");
-  iconElement.setAttibute(
-    "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}2x.png`
-  );
+  iconElement.src = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`;
 }
 
-displayDescription();
-
-// Need to check the API response for these paths
-
-function humidity(response) {
-  let humidity = document.querySelector("#humidity");
+function displayHumidity(response) {
+  let humidityElement = document.querySelector("#humidity");
   humidityElement.innerText = response.data.main.humidity;
 }
 
-displayHumidity();
-
-function wind(response) {
-  let wind = document.querySelector("#wind");
-  windElement.innerText = response.data.main.wind;
+function displayWind(response) {
+  let windElement = document.querySelector("#wind");
+  windElement.innerText = response.data.wind.speed;
 }
 
-displayWind();
-
-function precipitation(response) {
-  let precipitation = document.querySelector("precipitation");
-  precipitationElement.innerText = response.data.main.precipitation;
-}
-
-displayPrecipitation();
-
-function search(city) {
+async function search(city) {
   let apiKey = "f1bedc417492cf5cdfcb2aa86cb69d69";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-  axios.get(apiUrl).then(displayTemperature);
-}
+  let response = await axios.get(apiUrl);
+  displayTemperature(response);
+  displayCity(response);
+  displayDescription(response);
+  displayIcon(response);
+  displayWind(response);
+  displayHumidity(response);
 
-displayCity();
+  let apiUrl2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${response.data.coord.lat}&lon=${response.data.coord.lon}&exclude=current,minutely,hourly,alerts&appid=${apiKey}`;
+  console.log(apiUrl2);
+  response = await axios.get(apiUrl2);
+  displayForecast(response);
+}
 
 function searchText(event) {
   event.preventDefault();
@@ -66,93 +53,72 @@ function searchText(event) {
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", searchText);
 
-// let now = new Date();
-// let selecteddate = document.getElementById("date");
-// let date = now.getDate();
-// let days = [
-//   "Sunday",
-//   "Monday",
-//   "Tuesday",
-//   "Wednesday",
-//   "Thursday",
-//   "Friday",
-//   "Saturday",
-// ];
-// let day = days[now.getDay()];
-// let months = [
-//   "Jan",
-//   "Feb",
-//   "March",
-//   "April",
-//   "May",
-//   "June",
-//   "July",
-//   "August",
-//   "Sept",
-//   "October",
-//   "November",
-//   "December",
-// ];
-// let month = months[now.getMonth()];
-// let year = now.getFullYear();
+let now = new Date();
+let selecteddate = document.getElementById("selecteddate");
+let date = now.getDate();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let months = [
+  "Jan",
+  "Feb",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "Sept",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+let year = now.getFullYear();
 
-// selecteddate.innerHTML = `${day} ${month} ${date} ${year}`;
+document.getElementById(
+  "selecteddate"
+).innerHTML = `${day} ${month} ${date} ${year}`;
 
-function displayforecast() {
+function displayForecast(response) {
+  console.log(displayForecast);
+  let forecastHTML = "";
+  let forecast = response.data.daily;
+  forecast.forEach(function (forecastDay) {
+    forecastHTML += `<div class="weather-item">
+              <div class="weather-forecast-date">
+                 ${formatDay(forecastDay.dt)}
+              </div>    
+  <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}.png"
+               alt="Image"
+                width="42"
+                />
+                <div class="weather-forecast-temperature">
+            <span class="weather-forecast-temperature-max">
+               ${Math.round(forecastDay.temp.max)}ºC
+            </span>
+                <span class="weather-forecast-temperature-min">
+              ${Math.round(forecastDay.temp.min)}ºC
+                </span>
+               </span>
+               </div>
+       </div>`;
+  });
+
   let forecastElement = document.querySelector("#forecast");
-  let forecastHTML = `<div class="row>`;
-  forecastHTML =
-    forecastHTML +
-    `
-            <div class="col-2">
-            <div class="weather-forecast-date">
-              Mon
-            </div>
-            <div>
- <img src="https://cdn-icons-png.flaticon.com/512/1555/1555512.png" 
-              alt="Image"
-              width="42"
-              />
-              </div>
-              <div class="weather-forecast-temperature">
-            <span class="weather-forecast-temperature-max">
-              18ºC
-            </span>
-               <span class="weather-forecast-temperature-min">
-              12ºC
-               </span>
-              </span>
-              </div>
-        </div>
-  `;
-
-  forecastHTML =
-    forecastHTML +
-    `
-            <div class="col-2">
-            <div class="weather-forecast-date">
-              Mon
-            </div>
-            <div>
- <img src="https://cdn-icons-png.flaticon.com/512/1555/1555512.png" 
-              alt="Image"
-              width="42"
-              />
-              </div>
-              <div class="weather-forecast-temperature">
-            <span class="weather-forecast-temperature-max">
-              18ºC
-            </span>
-               <span class="weather-forecast-temperature-min">
-              12ºC
-               </span>
-              </span>
-              </div>
-        </div>
-  `;
-
-  let forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
 
-displayforecast();
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+  return days[day];
+}
